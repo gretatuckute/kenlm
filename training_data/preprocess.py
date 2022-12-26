783be7b9dce2e00e43694f41263edcb34ef9f8a6
Greta Tuckute <gretatu@mit.edu>
2022-12-26 17:14:57 -0500

    tokenization and preprocessing !

--- training_data/preprocess.py
+++ training_data/preprocess.py
@@ -8,8 +8,10 @@ for line in sys.stdin:
     # c += 1
     for sentence in nltk.sent_tokenize(line):
         # Strip punctuation, word_tokenize, lower-case
-        words = [w.strip(string.punctuation).lower() for w in nltk.word_tokenize(sentence)]
-        # print(' '.join(nltk.word_tokenize(sentence)).lower().strip(string.punctuation))
-        print(' '.join(words))
-    # if c > 10:
+        words = [w.strip(string.punctuation).lower() for w in nltk.wordpunct_tokenize(sentence)]
+        words_joined = " ".join(words)
+        # Get rid of several spaces
+        words_joined = " ".join(words_joined.split())
+        print(words_joined)
+    # if c > 30:
     #     break
